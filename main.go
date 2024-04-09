7429a86f3014b7a12e365ce6e147aeafeb2f2fba
Werwolfx <aasdvv@gmail.com>
2024-04-09 19:31:14 +0300

    add bearer and replicate

--- main.go
+++ main.go
@@ -9,14 +9,41 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type AIResponse struct {
-	Variants []string `json:"variants"`
+const replicateToken = "Bearer replicate.com"
+
+type Input struct {
+	TopK             int     `json:"top_k"`
+	TopP             float64 `json:"top_p"`
+	Prompt           string  `json:"prompt"`
+	Temperature      float64 `json:"temperature"`
+	MaxNewTokens     int     `json:"max_new_tokens"`
+	PromptTemplate   string  `json:"prompt_template"`
+	PresencePenalty  float64 `json:"presence_penalty"`
+	FrequencyPenalty float64 `json:"frequency_penalty"`
+}
+
+type AIRequest struct {
+	Input Input `json:"input"`
+}
+
+type AIErrorResponse struct {
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+	Status int    `json:"status"`
+}
+
+type AIResponseUri struct {
+	URLs struct {
+		Cancel string `json:"cancel"`
+		Get    string `json:"get"`
+	} `json:"urls"`
 }
 
 var (
@@ -77,7 +104,7 @@ func main() {
 	}
 }
 
-func getAISmsContent(prompt string, logger *log.Logger) (*AIResponse, error) {
+func getAISmsContent(prompt string, logger *log.Logger) (*AIResponseUri, error) {
 	// Call external AI service
 	aiResponse, err := callAIService(prompt, logger)
 	if err != nil {
@@ -87,7 +114,7 @@ func getAISmsContent(prompt string, logger *log.Logger) (*AIResponse, error) {
 	return aiResponse, nil
 }
 
-func callAIService(prompt string, logger *log.Logger) (*AIResponse, error) {
+func callAIService(prompt string, logger *log.Logger) (*AIResponseUri, error) {
 	// Check if corporate proxy is set
 	proxyURL, err := getProxyURL()
 	if err != nil {
@@ -103,14 +130,34 @@ func callAIService(prompt string, logger *log.Logger) (*AIResponse, error) {
 	}
 
 	// Call AI service
-	requestBody := map[string]string{"prompt": prompt}
+	requestBody := AIRequest{
+		Input: Input{
+			TopK:             50,
+			TopP:             0.9,
+			Prompt:           prompt,
+			Temperature:      0.6,
+			MaxNewTokens:     1024,
+			PromptTemplate:   "<s>[INST] {prompt} [/INST] ",
+			PresencePenalty:  0,
+			FrequencyPenalty: 0,
+		},
+	}
 	jsonBody, err := json.Marshal(requestBody)
 	if err != nil {
 		logger.Printf("Error marshaling request body: %v", err)
 		return nil, err
 	}
+	logger.Printf("Calling AI service with request body: %s", string(jsonBody))
 
-	resp, err := client.Post("https://api.example.com/generate-text", "application/json", bytes.NewBuffer(jsonBody))
+	req, err := http.NewRequest("POST", "https://api.replicate.com/v1/models/mistralai/mixtral-8x7b-instruct-v0.1/predictions", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		logger.Printf("Error creating request: %v", err)
+		return nil, err
+	}
+	req.Header.Add("Authorization", replicateToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		logger.Printf("Error calling AI service: %v", err)
 		return nil, err
@@ -122,15 +169,54 @@ func callAIService(prompt string, logger *log.Logger) (*AIResponse, error) {
 		logger.Printf("Error reading AI service response: %v", err)
 		return nil, err
 	}
+	logger.Printf("AI service response: %s", string(body))
 
-	var aiResponse AIResponse
-	err = json.Unmarshal(body, &aiResponse)
+	if resp.StatusCode != 201 {
+		logger.Printf("Error calling AI service: status code %d", resp.StatusCode)
+		var aiErrorResponse AIErrorResponse
+		err = json.Unmarshal(body, &aiErrorResponse)
+		if err != nil {
+			logger.Printf("Error unmarshaling AI service ERROR response: %v", err)
+			return nil, nil
+		}
+
+	}
+
+	var AIResponseUri AIResponseUri
+	err = json.Unmarshal(body, &AIResponseUri)
 	if err != nil {
-		logger.Printf("Error unmarshaling AI service response: %v", err)
+		logger.Printf("Error unmarshaling AI service response URI: %v", err)
 		return nil, err
 	}
 
-	return &aiResponse, nil
+	logger.Printf("result AI URI: %s", AIResponseUri.URLs.Get)
+
+	// Call AI service again
+	req, err = http.NewRequest("GET", AIResponseUri.URLs.Get, nil)
+	if err != nil {
+		logger.Printf("result Error creating req AI answer: %v", err)
+		return nil, err
+	}
+	req.Header.Add("Authorization", replicateToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	start := time.Now()
+	resp, err = client.Do(req)
+	elapsed := time.Since(start)
+	if err != nil {
+		logger.Printf("result Error calling AI service: %v (elapsed %s)", err, elapsed)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Printf("Error reading AI service response: %v (elapsed %s)", err, elapsed)
+		return nil, err
+	}
+	logger.Printf("result AI service response (elapsed %s): %s", elapsed, string(body))
+
+	return &AIResponseUri, nil
 }
 
 func getProxyURL() (*url.URL, error) {
